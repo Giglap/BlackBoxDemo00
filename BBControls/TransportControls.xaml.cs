aea1a7dd5615c2f27de4e33c65e05321d00b936b
Jim Crawford <jimcrawford@live.com>
2023-06-16 22:13:01 -0700

    Add project files.

---
+++ BBControls/TransportControls.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BBControls
+{
+    /// <summary>
+    /// Interaction logic for TransportControls.xaml
+    /// </summary>
+    public partial class TransportControls : UserControl
+    {
+        MediaElement _player = null;
+        double _naturalDuration = 0;
+        Timer VideoPoll;
+        public TransportControls()
+        {
+            InitializeComponent();
+            VideoPoll = new Timer(VideoPollCallback, null, 0, 500);
+        }
+        public void InitializeControls(MediaElement Player)
+        {
+            _player = Player;
+            _player.MediaOpened += OnMediaOpened;
+        }
+
+        private void VideoPollCallback(object? blahNull)
+        {
+            double elapsed;
+            try
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    elapsed = _player.Position.TotalSeconds;
+                    DisplayTime(elapsed, _naturalDuration);
+                });
+
+            }
+            catch (Exception)
+            {
+
+                //on shut down this sometimes errors.  It can be ignored
+            }
+ 
+        }
+
+        private void DisplayTime(double elapsed, double naturalDuration)
+        {
+           string Elapsed = convertSecondsToTimeString(elapsed);
+           string NaturalDuration = convertSecondsToTimeString(naturalDuration);
+            TimeDisplay.Text = $"     {Elapsed} / {NaturalDuration}";
+        }
+
+        private string convertSecondsToTimeString(double Seconds)
+        {
+            TimeSpan time = TimeSpan.FromSeconds(Seconds);
+            string timeString = time.ToString(@"hh\:mm\:ss");
+            return timeString;
+        }
+
+        private void OnMediaOpened(object sender, RoutedEventArgs e)
+        {
+            _naturalDuration = _player.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Pause();
+        }
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Play();
+        }
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Stop();
+        }
+        private void SkipBack_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Position = _player.Position.Add(new TimeSpan(0, 0, 0, -5, 0));
+
+        }
+        private void SkipFwd_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Position = _player.Position.Add(new TimeSpan(0, 0, 0, 5, 0));
+        }
+        private void ButtonDim(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).Opacity = .6;
+        }
+        private void ButtonBright(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).Opacity = 1;
+        }
+
+        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ((Button)sender).Margin = new Thickness(((Button)sender).Margin.Left + 25, ((Button)sender).Margin.Top + 25, 0, 0);
+        }
+
+        private void Button_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ((Button)sender).Margin = new Thickness(((Button)sender).Margin.Left - 25, ((Button)sender).Margin.Top - 25, 0, 0);
+        }
+    }
+}
---
+++ BlackBoxDemo00/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using BBControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlackBoxDemo00
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            VideoPlayer.Play();
+            VideoPlayer.IsMuted = true;
+            TransportControls.InitializeControls(VideoPlayer);
+        }
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MediaState state = GetMediaState(VideoPlayer);
+            if (e.Key == Key.T)
+            {
+                if (this.WindowStyle == WindowStyle.SingleBorderWindow)
+                {
+                    this.WindowStyle = WindowStyle.None;
+                }
+                else
+                {
+                    this.WindowStyle = WindowStyle.SingleBorderWindow;
+                }
+            }
+            if (e.Key == Key.F)
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                }
+            }
+            if (e.Key == Key.X)
+            {
+                if (state == MediaState.Play || state == MediaState.Pause)
+                {
+                    VideoPlayer.Position = VideoPlayer.Position.Add(new TimeSpan(0, 0, 0, 30, 0));
+                }
+            }
+            if (e.Key == Key.Z)
+            {
+                if (state == MediaState.Play || state == MediaState.Pause)
+                {
+                    VideoPlayer.Position = VideoPlayer.Position.Add(new TimeSpan(0, 0, 0, -30, 0));
+                }
+            }
+            if (e.Key == Key.P)
+            {
+
+
+                switch (state)
+                {
+                    case MediaState.Manual:
+                        VideoPlayer.Play();
+                        break;
+                    case MediaState.Play:
+                        VideoPlayer.Pause();
+                        break;
+                    case MediaState.Close:
+                        VideoPlayer.Play();
+                        break;
+                    case MediaState.Pause:
+                        VideoPlayer.Play();
+                        break;
+                    case MediaState.Stop:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (e.Key == Key.M)
+            {
+                VideoPlayer.IsMuted = !VideoPlayer.IsMuted;
+            }
+            if (e.Key == Key.Q)  // Hide transport controls
+            {
+               if(TransportControls.Visibility.ToString() == "Visible")
+                {
+                    TransportControls.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    TransportControls.Visibility = Visibility.Visible;
+                }
+            }
+        }
+        private MediaState GetMediaState(MediaElement myMedia)
+        {
+            FieldInfo hlp = typeof(MediaElement).GetField("_helper", BindingFlags.NonPublic | BindingFlags.Instance);
+            object helperObject = hlp.GetValue(myMedia);
+            FieldInfo stateField = helperObject.GetType().GetField("_currentState", BindingFlags.NonPublic | BindingFlags.Instance);
+            MediaState state = (MediaState)stateField.GetValue(helperObject);
+            return state;
+        }
+
+        private void TransportControls_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
